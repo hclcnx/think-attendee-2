fe1cac95dfb54ec7889cbd0584c11457458a1440
Brian Gleeson <brian.gleeson@ie.ibm.com>
2018-04-24 18:27:46 +0100

    Update helloWorld.user.js
    reset example

--- helloWorld/helloWorld.user.js
+++ helloWorld/helloWorld.user.js
@@ -47,7 +47,7 @@ if(typeof(dojo) != "undefined") {
             waitFor( function(){
 			// wait until the "loading..." node has been hidden
 			// indicating that we have loaded content.
-   			dojo.query("span.shareSome-title")[0].textContent="Check this out Again!";
+   			dojo.query("span.shareSome-title")[0].textContent="Hello World!";
        	          },
 		  ".lotusStreamTopLoading div.loaderMain.lotusHidden");
       } catch(e) {
