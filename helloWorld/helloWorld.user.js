a7e557223c62bccc0635251d9fa95cae05012c60
Brian Gleeson <brian.gleeson@ie.ibm.com>
2018-04-25 18:46:39 -0400

    Update helloWorld.user.js

--- helloWorld/helloWorld.user.js
+++ helloWorld/helloWorld.user.js
@@ -47,7 +47,7 @@ if(typeof(dojo) != "undefined") {
             waitFor( function(){
 			// wait until the "loading..." node has been hidden
 			// indicating that we have loaded content.
-   			dojo.query("span.shareSome-title")[0].textContent="Hello World!";
+   			dojo.query("span.shareSome-title")[0].textContent="Hello Social Connections!";
        	          },
 		  ".lotusStreamTopLoading div.loaderMain.lotusHidden");
       } catch(e) {
